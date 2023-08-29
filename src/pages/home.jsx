0614a8e91a46f30126d1ead53c4af3339e4462ba
Dakusy <dakusy@gmail.com>
2023-08-29 02:31:16 +0200

    first commit P14

---
+++ src/pages/home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Form from "../components/Form";
+
+
+const Homepage = () => {
+    return(
+        <div className="home_container">        
+            <p>test</p>
+        </div>
+    )
+}
+
+export default Homepage
