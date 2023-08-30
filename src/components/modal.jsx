90beebd9d260cd2986b18a046c940bf7a2450fab
Dakusy <dakusy@gmail.com>
2023-08-30 04:07:07 +0200

    modif answer from modal

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -4,7 +4,7 @@ function Modal({ closeModal }) {
     return (
         <div className={closeModal ? "modal_container" : "modal_display"}>
             <div className="modal_show">
-                <p>Employee created</p>
+                <p>Votre Employée a été enregistré.</p>
                 <div className="close_modal">
                     <p onClick={() => closeModal(false)}>X</p>
                 </div>
