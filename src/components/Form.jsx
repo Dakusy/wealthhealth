4ae290a5e1dadc0f5b6cf60de43a8185a5fa12b7
Dakusy <dakusy@gmail.com>
2023-09-25 18:34:33 +0200

    add verif on first and last name

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,25 @@
 import React, { useState } from "react";
 import Modal from "./modal";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 function Form() {
+
+    const dispatch = useDispatch();
+
     const [openModal, setOpenModal] = useState(false);
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [zipCode, setZipCode] = useState("");
+    const [birth, setBirth] = useState("");
+    const [date, setStart] = useState("");
+    const [street, setStreet] = useState("");
+    const [city, setCity] = useState("");
+    const [code, setCode] = useState("");
+    const [departement, setDepartement] = useState("");
+    const [state, setState] = useState("");
+    const [errors, setError] = useState(false)
+    const [dateToday] = useState(new Date().getTime())
 
     const handleFirstNameChange = (event) => {
         setFirstName(event.target.value);
@@ -21,23 +35,70 @@ function Form() {
 
     const handleClick = (event) => {
         event.preventDefault();
-        setOpenModal(true);
+        if(validate() === true){
+            event.stopPropagation()
+            //add data here to employee table
+            setOpenModal(true);
+        }else {
+            alert("stop")
+        }
         console.log("Prénom:", firstName);
         console.log("Nom:", lastName);
         console.log("Code postal:", zipCode);
     };
 
+    const nameValidation = /^[A-Za-zÀ-ÖØ-öø-ÿ- -]{1,}$/;
+    const numberValidation = /^[0-9]{6}/;
+
+    const dataEmployee = {
+        firstName,
+        lastName,
+        birth: JSON.stringify(birth),
+        date: JSON.stringify(date),
+        street,
+        city,
+        code,
+        departement,
+        state,
+    };
+
+    const validate = () => {
+        if (nameValidation.test(firstName.trim()) &&
+            nameValidation.test(lastName.trim()) &&
+            state !== ""
+        ) {
+            return true
+        }
+
+
+        else {
+            return setError(true)
+        }
+
+    }
+
     return (
         <div>
+            <Link to="/Employee">View Current Employees</Link>
             <h2>Formulaire</h2>
             <form onSubmit={handleClick}>
                 <div>
                     <label>Prénom:</label>
                     <input type="text" value={firstName} onChange={handleFirstNameChange} />
+                    {errors && !nameValidation.test(firstName.trim()) ? (
+                        <p className="error_input">Votre prenom doit comporter au minimum 1 caractère</p>
+                    ) : (
+                        ""
+                    )}
                 </div>
                 <div>
                     <label>Nom:</label>
                     <input type="text" value={lastName} onChange={handleLastNameChange} />
+                    {errors && !nameValidation.test(lastName.trim()) ? (
+                        <p className="error_input">Votre nom doit comporter au minimum 1 caractère</p>
+                    ) : (
+                        ""
+                    )}
                 </div>
                 <div>
                     <label>Code postal:</label>
